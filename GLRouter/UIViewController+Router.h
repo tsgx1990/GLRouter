ad486346dfe7b3da96a839b0baabc3a53eea3897
luguanglong <luguanglong@syswin.com>
2016-12-07 22:58:43 +0800

    add comment

--- GLRouter/UIViewController+Router.h
+++ GLRouter/UIViewController+Router.h
@@ -32,8 +32,23 @@
 
 @property (nonatomic, strong, readonly) NSDictionary* router_params;
 
+/*
+ routerUrl      一般的形式为：schema://host/key1/value1/key2/value2/key3/value3
+                目前只支持两种协议：
+                router:// 协议对应的 host 是控制器类名
+                clsmap:// 协议对应的 host 是 router_controllerClassMapping 中的key
+                如果routerUrl只传了一个控制器类名，则直接跳转到该控制器界面。
+ 
+ paramsBlock    用于返回传递参数给controller，当为nil时，表示不传递参数
+ 
+ openModeBlock  用于自定义跳转方式，当为nil时，会采用默认的跳转方式
+ */
 - (UIViewController*)router_openController:(NSString*)routerUrl paramsBlock:(NSDictionary*(^)(UIViewController* controller))paramsBlock openModeBlock:(void(^)(UIViewController* controller))openModeBlock;
 
+
+/*
+ closeModeBlock     用于自定义界面退出方式，当为nil时，会采用默认的退出方式
+ */
 - (void)router_closeControllerAnimated:(BOOL)animated closeModeBlock:(void(^)())closeModeBlock;
 
 
